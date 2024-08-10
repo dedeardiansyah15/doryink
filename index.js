adb1a57f0571988f888368b5e8104d54f8d7de96
dedeardiansyah15 <dede.ardiansyah77@outlook.com>
2024-08-10 19:25:54 +0800

    progres 2

--- index.js
+++ index.js
@@ -6,3 +6,48 @@ function sidebarToggleFunction(){
 }
 
 sidebarToggleFunction()
+
+
+window.addEventListener('scroll',function(){
+    const header = document.getElementById('header');
+
+    if (window.scrollY >50){
+        header.classList.add('scrolled');
+    }else{
+        header.classList.remove('scrolled');
+    }
+});
+
+
+const image = document.getElementById('image');
+const button = document.getElementById('toggleButton');
+
+const images  = [
+    'asset/home/slider/1.png',
+    'asset/home/slider/2.png',
+    'asset/home/slider/3.png',
+    'asset/home/slider/4.png',
+    'asset/home/slider/5.png',
+    'asset/home/slider/6.png'
+];
+
+let currentIndex = 0;
+
+function changeImage(){
+
+    image.classList.remove('active');
+
+    setTimeout(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+        image.src=images[currentIndex];
+
+
+        setTimeout(() => {
+            image.classList.add('active');
+        }, 10);
+    }, 500);
+}
+
+button.addEventListener('click', changeImage);
+
+image.classList.add('active');
